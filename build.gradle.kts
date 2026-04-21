5e90b571c3f28706ae2bab897719aac2940a1640
Diego Braun da Cunha <11878263+diegobcunha@users.noreply.github.com>
2026-04-21 19:12:29 -0300

    feature:: Add firebase support to project. (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kover)
+    alias(libs.plugins.google.services) apply false
 }
 
 dependencies {
